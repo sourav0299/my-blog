1c8659b819ab8f1a818312ba9c9105582114af99
sourav0299 <sourav2000kumar07@gmail.com>
2024-05-09 01:48:17 +0530

    updated edit and delete functionality

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,7 +7,30 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
+    public function deletePost(Post $post){
+        if(auth()->user()->id === $post['user_id']){
+            $post->delete();
+        }
+        return redirect('/');
+    }
+    public function updatePost(Post $post, Request $request){
+        if(auth()->user()->id !== $post['user_id']){
+            return redirect('/');
+        }
+        $incomingFields = $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+
+        $post->update($incomingFields);
+        return redirect('/');
+    }
     public function editPost(Post $post){
+        if(auth()->user()->id !== $post['user_id']){
+            return redirect('/');
+        }
         return view('edit-post', ['post' => $post]);
     }
     public function createPost(Request $request){
